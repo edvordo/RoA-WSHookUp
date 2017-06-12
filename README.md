07224f6856a799625df901c084c94663b77fca40
edvordo <edo@edvordo.sk>
2017-06-12 13:36:30 +0200

    added page-specific events, updated README.md

--- README.md
+++ README.md
@@ -1,25 +1,54 @@
 # RoA-WSHookUp
 A simple script to hook to the games' WebSocket connection an broadcasts the received messages in custom events
 
+## Changelog
+
+2017-06-12 13:06 - v0.3
+ * addded `roa-ws:page:*` events
+
+2017-05-30 11:28 - v0.2
+ * initial version
+
 ## About
 
 ### Available events
 Pick one speciffic to listen to from. All are prepended with `roa-ws:`
 
  * `battle`
- * `trade`
+ * `harvests`
  * `gamestats`
  * `mychans`
  * `motd`
  * `login_info`
  * `message`
  * `notification`
- * `page`
+ * `page` - *the `roa-ws:page` event is still broadcasted, following are additional events for if you'd want to monitor only a speciffic page:*
+   * `page:updates`
+   * `page:town`
+   * `page:town_battlegrounds`
+   * `page:house`
+   * `page:house_room`
+   * `page:house_room_item` - *`data.item` contains info about a given room*
+   * `page:clans`
+   * `page:clan_members`
+   * `page:clan_treasury` - *Funds*
+   * `page:clan_buildings`
+   * `page:training`
+   * `page:boosts`
+   * `page:quests`
+   * `page:inventory_items` - *Same for Weapon and Armor, the `data.item_type` tells you which .. `weapons` / `armor`*
+   * `page:inventory_tools`
+   * `page:inventory_gems`
+   * `page:inventory_accessories`
+   * `page:inventory_ingredients`
+   * `page:inventory_consumables`
+   * `page:settings_milestones` - *for all Milestones, `data.milestone_type` tells you which page .. `PREMIUM` / `FAME` / `LEVEL` / `[TS type]_LEVEL` etc. and `PROGRESS_CHECK` for an overview*
 
 
-^ Those are the ones I know of, there probably are and will be more.
+^ These are just examples and, while an extensive list, there are more. Inspect the websocket frames to find more.
 
 _Note: "general" include events that do not have a type, if there are any._
+
 ### Example code
 
 From your userscript you can listen to these custom events with the following examples
@@ -37,6 +66,11 @@ $(document).on("roa-ws:trade", function(e, data){
 
 // chat messages
 $(document).on("roa-ws:message", function(e, data){
+    console.log(data); // data has parsed JSON data from the WebSocket event
+});
+
+// Crystal Shop
+$(document).on("roa-ws:page:boosts", function(e, data){
     console.log(data); // data has parsed JSON data from the WebSocket event
 });
 
--- RoA-WSHookUp.user.js
+++ RoA-WSHookUp.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         RoA WSHookUP
 // @namespace    Reltorakii_is_awesome
-// @version      0.2
-// @description  try to take over the world!
+// @version      0.3
+// @description  Hooks to the websocket communication and broadcasts the various messages as custom events
 // @author       Reltorakii
 // @run-at       document-start
 // @match        https://beta.avabur.com/game
@@ -13,7 +13,7 @@
 
 (function(window) {
     'use strict';
-    
+
     var OriginalWS = window.WebSocket;
     var initOWS    = OriginalWS.apply.bind(OriginalWS);
     var wsListener = OriginalWS.prototype.addEventListener;
@@ -40,13 +40,35 @@
                 data = JSON.parse(event.data);
                 for (var i = 0; i < data.length; i++) {
                     etype = "roa-ws:";
+                    var etypepage = "";
                     var item = data[i];
                     if (item.hasOwnProperty("type")) {
                         etype = etype + item.type;
+                        // in case its a "page" type message create additional event
+                        // e.g.: "roa-ws:page:boosts", "roa-ws:page:clans" or "roa-ws:page:settings_milestones" etc.
+                        if (item.type === "page" && item.hasOwnProperty("page") && "string" === typeof item.page) {
+                            etypepage = etype + ":" + item.page;
+                        }
                     } else {
                         etype = etype + "general";
                     }
+
+                    // This is a special kind of message containing info about the user
+                    // I'm resending it in 5 second because this script is supposed to
+                    // run at "document start" and potential userscripts that are
+                    // listening to the even "roa:login_info" may not catch this message.
+                    // They should, I got the message 99 times out of 100, which shows
+                    // that userscripts are not 100% reliable with their running queue.
+                    if (etype === "roa-ws:login_info") {
+                        setTimeout(function(_etype, _item){
+                            $(document).trigger(_etype, _item);
+                        }, 5000, etype, item);
+                    }
+
                     $(document).trigger(etype, item);
+                    if (etypepage.length > 0) {
+                        $(document).trigger(etypepage, item);
+                    }
                 }
             } catch (ex) {
                 data = event.data;
@@ -66,5 +88,5 @@
         if (hooked === false) {
             $.alert("The script 'WSHookUp' failed to hook to the WebSocket connection Avabur uses. In case you are using another userscript that depends on this, you may want to refresh the page to try again.", "WSHookUp Failed");
         }
-    }, 1000);
+    }, 30000);
 })(window);
